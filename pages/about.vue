184cab639c7d60b4d4cf7972fe0f0c1e65ae383a
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2022-12-22 12:23:42 +0600

    test

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>I am about</h1>
+    <NuxtLink to="/">Back</NuxtLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { data } = await useFetch("/api/hello");
+</script>
+
+<template>
+  <div>
+    <h1>Hello Nuxt</h1>
+    {{ data?.foo }}
+    <NuxtLink to="/about">About</NuxtLink>
+  </div>
+</template>
